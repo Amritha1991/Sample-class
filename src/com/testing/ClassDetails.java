68bcb6fcc4661ad7d7c230132ddd4e94638f0a6f
Amritha1991 <sakthiamiru@gmail.com>
2021-04-01 16:51:21 +0530

    done

---
+++ src/com/testing/ClassDetails.java
@@ -0,0 +1,22 @@
+package com.testing;
+
+public class ClassDetails {
+
+	public void morningClass() {
+System.out.println("Class at 8 am");
+ 
+	}
+	
+public void classmembers() {
+	System.out.println("9members in 8am");
+
+}
+ public static void main(String[] args) {
+	
+	 ClassDetails java= new ClassDetails();
+	 
+	java.morningClass();
+    java.classmembers();
+ }
+}
+
